eae5e883081ea44737019c234fe2e924098a91e7
Adriany Souza <adrianysouzaa@gmail.com>
2020-01-30 18:30:33 -0400

    Criação do formulário categoria e criação da tabela categoria no Banco de Dados

---
+++ _inserir_categoria.php
@@ -0,0 +1,11 @@
+<?php
+
+include 'conexao.php';
+
+$categoria= $_POST['categoria'];
+
+$sql= "INSERT INTO categoria (categoria) values ('$categoria')";
+$inserir= mysqli_query($conexao, $sql);
+
+
+?>
--- adicionar_categoria.php
+++ adicionar_categoria.php
@@ -1,38 +1,46 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Menu</title>
+	<title></title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+	<style type="text/css">
+		#botao{
+			background-color: #FF1168; /* cor de fundo */
+			color: #ffffff; /* cor da letra */
+		}
+
+	</style>
 
 </head>
 <body>
 
-<div class="container" style="margin-top: 100px">
-	<div class="row">
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Adicionar Produto</h5>
-        <p class="card-text">Opção para adicionar produtos em nosso estoque.</p>
-        <a href="adicionar_produto.php" class="btn btn-primary">Cadastrar Produto</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Lista de Produtos</h5>
-        <p class="card-text">Visualizar, editar e excluir os produtos.</p>
-        <a href="listar_produtos.php" class="btn btn-primary">Produtos</a>
-      </div>
-    </div>
-  </div>
+<div class="container" style="margin-top: 40px; width: 500px">
+
+	 <div style="text-align: right">
+	    <a href="index.php" role="button" class="btn  btn-sm btn-primary">Voltar</a>	
+	 </div>
+
+	<h4>Cadastro Categoria</h4>
+	<form action="_inserir_categoria.php" method="post">
+		<label>Categoria</label>
+		<div class="form-group">
+			<input type="text" name="categoria" class="form-control" placeholder="Digite o nome da Categoria" autocomplete="off">
+		</div>
+		<div style="text-align: right">
+			<button type="submit" id="botao" class="btn btn-sm">Cadastrar </button>
+		</div>
+	</form>
 </div>
+
+
+
+
+
 
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
 
 
 </body>
